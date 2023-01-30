01855e54d9368cbde8cbfc07d5357b1e2e933e64
Yaroslav Tsy <is2419227@gmail.com>
2023-01-30 23:50:30 +0200

    fix extra bugs

--- js/modules/showAboutText.js
+++ js/modules/showAboutText.js
@@ -1,13 +1,28 @@
 // Showing-Disabled More Text in Section "About" //
-
+const idAboutInfo = document.getElementById('aboutInfo').id;
 const textWrapper = document.querySelector('.about-text');
 const openTextLink = document.querySelector('.more');
 const textOnClick = document.querySelectorAll('.show');
+const idTextOnClickFirst = document.getElementById('showFirst').id;
 const openTextArrow = document.querySelector('.picture__fix');
 
+console.log(idTextOnClickFirst.id);
+console.log(idAboutInfo.id);
+
+function openHref() {
+	if (textWrapper.classList.contains('not-active')) {
+		openTextLink.href = `#${idTextOnClickFirst}`
+	} else {
+		openTextLink.href = `#${idAboutInfo}`
+	}
+}
+
+openHref();
+
 openTextLink.addEventListener('click', function () {
-    textWrapper.classList.toggle('not-active');
-    textOnClick.forEach(el => el.classList.toggle('disabled'));
-    openTextArrow.classList.toggle('active');
+	openHref();
+	textWrapper.classList.toggle('not-active');
+	textOnClick.forEach(el => el.classList.toggle('disabled'));
+	openTextArrow.classList.toggle('active');
 })
 ////
